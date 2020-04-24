978e11ff150e1ae68bd7574c7e69f49b1232c503
STROEML <lukas_jan.stroemsdoerfer@daimler.com>
2020-04-24 08:50:27 +0200

    Included description in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='kubipy',
-      version='0.0.1',
+      version='0.1',
       description='a simply library to orchestrate local kubernetes clusters from python',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/LJStroemsdoerfer/kubipy',
       author='Lukas Jan Stroemsdoerfer',
       author_email='ljstroemsdoerfer@gmail.com',
       license='MIT',
       packages=['kubipy'],
       install_requires=[
-          'requests'
+          'requests',
+          'subprocess'
       ],
       zip_safe=False)
